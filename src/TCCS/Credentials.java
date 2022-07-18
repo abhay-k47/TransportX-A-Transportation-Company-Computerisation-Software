b960040917933e8c2a2f4d306d0823395c6be105
abhay-k47 <99312837+abhay-k47@users.noreply.github.com>
2022-07-18 12:06:13 +0530

    Update Credentials.java

--- src/TCCS/Credentials.java
+++ src/TCCS/Credentials.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 
 public class Credentials {
     public static String url = "jdbc:mysql://localhost:3306/mysql";
-    public static String user = "root";
-    public static String password = "@mysql#123";
+    public static String user = "user_name";
+    public static String password = "password";
     public static SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY/MM/DD");
 }
